47ca4afcf8928224589ed72908e821aa7fe413e2
William <willspri@gmail.com>
2025-02-21 14:34:14 +1300

    updated

--- textInsights.py
+++ textInsights.py
@@ -1,5 +1,7 @@
-#count distinct words
+#total word count 
 
+
+#count distinct words
 def count_distinct(filename):
     with open(filename, 'r') as file:
     #to lower to count cases as same word
@@ -14,8 +16,11 @@ def count_distinct(filename):
 text_sample = "sample.txt"
 
 count_number = count_distinct(text_sample)
-
-#print(f"There are {count_number} distinct words in the sample")
-#count_number = count_distinct(text_sample)
-
-print(f"There are {count_number} distinct words in the sample")
+if count_number == 1:
+    print(f"There is {count_number} distinct word in the sample")
+if count_number > 1:
+    print(f"There are {count_number} distinct words in the sample")
+if count_number == 0:
+    print("No words found in sample")
+#else: 
+    #print("No words or something else went wrong")
